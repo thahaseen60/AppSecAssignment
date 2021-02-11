749cf415866cbdc561a9954c47738df0a89cee9c
THAHASEEN I <180876X@mymail.nyp.edu.sg>
2021-02-11 11:25:27 +0800

    Till password

---
+++ AppSecAssignment/Registration.aspx
@@ -0,0 +1,91 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Registration.aspx.cs" Inherits="AppSecAssignment.Registration" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>My Registration</title>
+     
+    <script type="text/javascript">
+        function validate() {
+            var str = document.getElementById('<%=tb_password.ClientID %>').value;
+            // password check for less than 8 characters
+            if (str.length < 8) {
+                document.getElementById("lbl_pwdchecker").innerHTML = "Password Length Must be at Least 8 Characters";
+                document.getElementById("lbl_pwdchecker").style.color = "Red";
+                return ("too_short");
+            }
+
+            // password check for at least 1 number
+            else if (str.search(/[0-9]/) == -1) {
+                document.getElementById("lbl_pwdchecker").innerHTML = "Password require at least 1 number";
+                document.getElementById("lbl_pwdchecker").style.color = "Red";
+                return ("No_number");
+            }
+
+            // password check for lower case
+            else if (str.search(/[a-z]/) < 0) {
+                document.getElementById("lbl_pwdchecker").innerHTML = "Password require at least 1 lower case letter";
+                document.getElementById("lbl_pwdchecker").style.color = "Red";
+                return ("No_lowercase");
+            }
+
+            // password check for upper case
+            else if (str.search(/[A-Z]/) < 0) {
+                document.getElementById("lbl_pwdchecker").innerHTML = "Password require at least 1 upper case letter";
+                document.getElementById("lbl_pwdchecker").style.color = "Red";
+                return ("No_uppercase");
+            }
+
+            // password check for special characters
+            else if (str.search(/[!@#$%^&*./]/) == -1) {
+                document.getElementById("lbl_pwdchecker").innerHTML = "Password require at least 1 special character. You can choose any special character from here: ( ! @ # $ % ^ & * . / ) ";
+                document.getElementById("lbl_pwdchecker").style.color = "Red";
+                return ("No_specialcharacter");
+            }
+
+            document.getElementById("lbl_pwdchecker").innerHTML = "Excellent!";
+            document.getElementById("lbl_pwdchecker").style.color = "Blue";
+        }
+    </script>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Registration<br />
+            <br />
+            First Name
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Last Name
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Credit Card Info
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Email Address
+            <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Password&nbsp;&nbsp;
+            <asp:TextBox ID="tb_password" runat="server" Width="206px" onkeyup="javascript:validate()" TextMode="Password"></asp:TextBox>
+&nbsp;
+            <asp:Label ID="lbl_pwdchecker" runat="server" Text="pwdchecker"></asp:Label>
+            <br />
+            <br />
+            Date of Birth
+            <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="btn_checkPassword" runat="server" Text="Check Password" Width="365px" />
+            <br />
+            <br />
+            <asp:Button ID="btn_Submit" runat="server" Text="Submit" Width="364px" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ AppSecAssignment/Registration.aspx.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions; // for regular expression
+using System.Drawing; // for change of color
+using System.Security.Cryptography;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace AppSecAssignment
+{
+    public partial class Registration : System.Web.UI.Page
+    {
+        private object lbl_pwdchecker;
+        private readonly object tb_password;
+
+        string MYDBConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["APPSECDBConnection"].ConnectionString;
+        static string finalHash;
+        static string salt;
+        byte[] Key;
+        byte[] IV;
+
+        /*
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        */
+
+        protected void btn_Submit_Click(object sender, EventArgs e)
+        {
+            //string pwd = get value from your Textbox
+            string pwd = tb_password.ToString().Trim();
+
+            //Generate random "salt"
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            byte[] saltByte = new byte[8];
+
+            //Fills array of bytes with a cryptographically strong sequence of random values.
+            rng.GetBytes(saltByte);
+            salt = Convert.ToBase64String(saltByte);
+            SHA512Managed hashing = new SHA512Managed();
+            string pwdWithSalt = pwd + salt;
+            byte[] plainHash = hashing.ComputeHash(Encoding.UTF8.GetBytes(pwd));
+            byte[] hashWithSalt = hashing.ComputeHash(Encoding.UTF8.GetBytes(pwdWithSalt));
+            finalHash = Convert.ToBase64String(hashWithSalt);
+            RijndaelManaged cipher = new RijndaelManaged();
+            cipher.GenerateKey();
+            Key = cipher.Key;
+            IV = cipher.IV;
+            //createAccount();
+        }
+
+        protected void btn_checkPassword_Click(object sender, EventArgs e)
+        {
+            // implement codes for the button event
+            // Extract data from textbox
+            int scores = checkPassword(tb_password);
+            string status = "";
+            switch (scores)
+            {
+                case 1:
+                    status = "Very Weak";
+                    break;
+                case 2:
+                    status = "Weak";
+                    break;
+                case 3:
+                    status = "Medium";
+                    break;
+                case 4:
+                    status = "Strong";
+                    break;
+                case 5:
+                    status = "Excellent";
+                    break;
+                default:
+                    break;
+            }
+            lbl_pwdchecker = "Status : " + status;
+            if (scores < 4)
+            {
+                lbl_pwdchecker = Color.Red;
+                return;
+            }
+            lbl_pwdchecker = Color.Green;
+        }
+
+        private int checkPassword(object tb_password)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*
+        private int checkPassword(object text)
+        {
+            throw new NotImplementedException();
+        }
+        */
+
+        private int checkPassword(string password)
+            {
+
+                int score = 0;
+
+                // Score 1 = Very Weak
+                if (password.Length < 8)
+                {
+                    return 1;
+                }
+                else
+                {
+                    score = 1;
+                }
+
+                // Score 2 = Weak
+                if (Regex.IsMatch(password, "[a-z]"))
+                {
+                    score = 2;
+                }
+
+                // Score 3 = Medium
+                if (Regex.IsMatch(password, "[A-Z]"))
+                {
+                    score = 3;
+                }
+
+                // Score 4 = Strong
+                if (Regex.IsMatch(password, "[0-9]"))
+                {
+                    score = 4;
+                }
+
+                // Score 5 = Excellent
+                if (Regex.IsMatch(password, "[!@#$%^&*./]"))
+                {
+                    score = 5;
+                }
+
+                return score;
+
+            }
+        }
+    }
